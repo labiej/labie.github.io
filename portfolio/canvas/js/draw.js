682eed7aff566c062ba0ad3961485680ba8a5e6c
Joris Labie <joris.labie1@gmail.com>
2017-01-06 15:21:16 +0100

    Demonstration of current capabilities

---
+++ portfolio/canvas/js/draw.js
@@ -0,0 +1,6 @@
+MyCanvas.flipY( 1 );
+
+MyCanvas.drawAxes();
+MyCanvas.drawCircle( 0, 0, 15, { fill: "#F0F" });
+MyCanvas.drawCircle( 0, 0, 30);
+MyCanvas.drawCircle( 15, 15, 35, {stroke: "#FFF", fill: "rgba(255,0,0,0.3)"});
