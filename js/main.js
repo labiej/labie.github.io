1480d19f7935ac32c86885fc63f8586a724e5c21
Joris Labie <joris.labie1@gmail.com>
2017-01-09 19:26:26 +0100

    Add javascript for delayed animation for gauges

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+/* Set length of arc in svg */
+
+function setProgress( $circle, unset = false){
+  var val = !unset ? $circle.parent().parent().attr('data-pct')
+									 : 0;
+	console.log( !unset ? 'inside viewport' : 'outside' );
+	console.log( "value: " + val );
+  var radius = $circle.attr('r');
+  var circumference = Math.PI*(radius*2);
+
+	var strokeOffset;
+
+  if (val < 0) {
+		val = 0;
+		$circle.parent().parent().attr('data-pct',val);
+	}
+  if (val > 100) {
+		val = 100;
+		$circle.parent().parent().attr('data-pct',val);
+	}
+
+  strokeOffset = ((100-val)/100)*circumference;
+
+  $circle.css({ strokeDashoffset: strokeOffset});
+
+
+}
+
+/* INITIALISE LISTENERS */
+$(document).ready(function(){
+  var i;
+
+  $('.scrollspy').scrollSpy();
+
+  $('.progress > .svg > .bar').inViewport(
+		function(){ console.dir($(this)); setProgress( $(this) ) },
+		function() { console.dir($(this)); setProgress( $(this), true)}
+	);
+
+  /* Rotate logo once */
+  $('.svg-logo').addClass("rotator");
+
+
+});
