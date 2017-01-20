6f881da1a8c9ab5e8af8107b7adea2f9d9ea7cb6
Joris Labie <joris.labie1@gmail.com>
2017-01-20 18:06:47 +0100

    Add common options to accordion implementation

--- portfolio/tribute/js/index.js
+++ portfolio/tribute/js/index.js
@@ -17,39 +17,71 @@ var accordion = (function( $, container ) {
 	};
 
 	// Public facing functions
+
+	// Initialize accordion
 	var init = function() {
 		// Set parameters if data attributes are defined
 		_multiExpand = _setIfDefined( _container.data('multi-expand'), _multiExpand );
 		_collapseAll = _setIfDefined( _container.data('collapse-all'), _collapseAll );
 
+		$( 'button.accordion-title.active' ).one( 'click', closeContent );
+		$( 'button.accordion-title:not(.active)' ).one( 'click', openContent );
 
 	};
 
+	// Get and Set settings
+
+	var getSettings = function() {
+		return {
+			"multi-expand": _multiExpand,
+			"collapse-all": _collapseAll
+		};
+	};
+
+	var setSettings = function( newSettings ) {
+		if ( typeof newSettings !== "object" )
+			return null;
+
+		// Insert new settings
+		_multiExpand = _setIfDefined( newSettings[ 'multi-expand' ], _multiExpand );
+		_collapseAll = _setIfDefined( newSettings[ 'collapse-all' ], _collapseAll );
+
+		// Change data-* attributes
+		_container.data( 'multi-expand', _multiExpand );
+		_container.data( 'collapse-all', _collapseAll );
+
+		// Return the new settings
+		return getSettings();
+	};
+
+	// Accordion functionality
 	var openContent = function() {
 		// Use clear names
 		var $button = $( this );
 		var $content = $button.next();
 
-		// Open current content and highlight tab
+		// Open current content and highlight tab...
 		$button.addClass( 'active' );
 		$content.slideDown();
 
-		// Attach close content handler
+		// Attach close content handler:
 		$button.one( 'click', closeContent );
 
-		// Apply multi-expand rules
-
-		$( 'button.active' ).not( $button ).removeClass( 'active' );
-		$( '.accordion-content' ).not( $content ).slideUp();
+		// Apply multi-expand rules...
+		if( !_multiExpand ){
+			$( 'button.active' ).not( $button ).removeClass( 'active' );
+			$( '.accordion-content' ).not( $content ).slideUp();
+		}
 
 		// Filthy hack to remove outline ( FIX WITH CSS );
-		// $button.blur();
+		$button.blur();
 	};
 
 	var closeContent = function() {
 		// Use clear names
 		var $button = $( this );
 		var $content = $button.next();
+		var $allButtons = $( 'button.accordion-title' );
 
 		// Close content and remove active class
 		$content.slideUp( '' );
@@ -59,8 +91,12 @@ var accordion = (function( $, container ) {
 		$button.one( 'click', openContent );
 
 		// Show first entry if collapse-all is set to FALSE
+		if( !_collapseAll && $( 'button.accordion-title.active').length === 0 ){
+			$allButtons[0].click();
+		}
 
-
+		// Remove focus after click
+		$button.blur();
 	};
 
 	var dbgStorage = function() {
@@ -72,20 +108,14 @@ var accordion = (function( $, container ) {
 	// Reveal public members of module
 	return {
 		init: init,
+		getSettings: getSettings,
+		setSettings: setSettings,
 		openContent: openContent,
-		closeContent: closeContent,
+		//closeContent: closeContent,
 		dbgStorage: dbgStorage
 	};
 
 } );
 
-
-
-
 var accordionHandler = accordion( jQuery );
 accordionHandler.init();
-
-accordionHandler.dbgStorage();
-
-$( 'button.accordion-title.active' ).one( 'click', accordionHandler.closeContent );
-$( 'button.accordion-title:not(.active)' ).one( 'click', accordionHandler.openContent );
